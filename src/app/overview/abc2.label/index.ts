cc12ad16f8984319c231f1e4f776fbb602fef18e
Csini <mocorgo@maffia.hu>
2016-05-08 01:51:53 +0200

    field pipe is working and label is there without style and tooltip

---
+++ src/app/overview/abc2.label/index.ts
@@ -0,0 +1,1 @@
+export * from './abc2.label.component';
--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,17 +1,33 @@
 import {Component} from 'angular2/core';
 import {Tree} from './tree';
+import {Abc2Label} from './abc2.label';
+import {FieldConvertPipe} from './abc2.field';
 
 @Component({
   selector: 'overview',
   providers: [],
-  directives: [Tree],
-  pipes: [],
+  directives: [Tree, Abc2Label],
+  pipes: [FieldConvertPipe],
   styles: [require('./overview.component.scss')],
   template: require('./overview.html')
 })
 export class Overview {
+
+  public fields: string[];
+
   constructor() { }
 
+
   ngOnInit() {
+    this.fields = [];
+    this.fields[0] = 'id';
+    this.fields[1] = 'name';
+  }
+
+  getManualEntry(): Object {
+    return { id: 11, name: 'manual Nameee' };
+  }
+  getAutoEntry(): Object {
+    return { id: 22, name: 'AUTO Name' };
   }
 }
